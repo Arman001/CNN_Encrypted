b565be67882db50bbedad50910abcb2a77b19814
Arman001 <ghost@debian-BULLSEYE-live-builder-AMD64>
2022-04-23 11:18:39 +0500

    Improvements in speed and accuracy

---
+++ main.py
@@ -0,0 +1,61 @@
+import numpy as np
+from parameters_generation import SEALWork
+from convolution import Convolution
+from pooling import Pooling
+from output import Output
+import time
+# from data_preprocessing import load_data
+import pickle
+
+
+def main():
+    seal_obj = SEALWork()
+    seal_tuple = seal_obj.initialize()
+    encryptor, evaluator, decryptor, slot_count, context, ckks_encoder, scale, galois_keys, relin_keys = seal_tuple
+    print("Total slots available: ", slot_count)
+    print(".........Loading the Data........")
+    class_labels = ["cridex", "smb"]
+    with open('./Data/Test_X2.pkl', 'rb') as file:
+        X = pickle.load(file)
+    with open('./Data/Test_Y2.pkl', 'rb') as file:
+        Y = pickle.load(file)
+
+    # X = X / 255.0
+    print(f"Classes are: {class_labels}")
+    print(f"Total Data is: {len(X)}")
+    print("----------------------------------------")
+    print("------Testing of HE CNN is started------")
+
+    input_size = 28
+
+    con = Convolution(8, seal_tuple, input_size)
+    pool = Pooling(2, seal_tuple, 14, 8, 28)
+    output = Output(392, 112, 0, seal_tuple)
+
+    acc = 0
+    print("----------------------------------------")
+    print("Samples Used for Testing: 1000")
+    print("----------------------------------------")
+    start = time.time()
+    for i in range(1000):
+        checker = i
+        # print("Real Prediction = ", Y[checker])
+        plain_input = ckks_encoder.encode(X[checker], scale)
+        input_cipher = encryptor.encrypt(plain_input)
+        out, out_size, output_gap = con.Convolve(input_cipher)
+        out_size, rotations, out, output_gap = pool.Mean_Pool(out)
+        out = output.Final_Calculations(out)
+        if(out[0] == Y[checker][0]):
+            acc = acc + 1
+        if(i != 0 and (i + 1) % 200 == 0):
+            print(f"{i + 1} items done!")
+    end = time.time()
+    print(f"Total time {end-start} seconds")
+    print("----------------------------------------")
+    print("Accurately Predicted items: ", acc)
+    print(f"Accuracy is: {(acc / 1000) * 100}%")
+    print("----------------------------------------")
+
+
+if __name__ == "__main__":
+    main()
---
+++ output.py
@@ -0,0 +1,115 @@
+# ......................... #
+# Created By Muhammad Saad #
+# On 30/03/2022 #
+# At 7:15 AM #
+# .......... #
+
+from convolution import Level_Checker
+import numpy as np
+import pickle
+
+
+class Output:
+
+    # Initilization of Output class
+    def __init__(self, input_size, rotations, input_len, seal_tuple):
+        self.encryptor, self.evaluator, self.decryptor, self.slot_count, self.context, self.ckks_encoder, self.scale, self.galois_keys, self.relin_keys = seal_tuple
+        self.input_size = int(input_size)
+        with open('./Data/Approximate88/fc_weights.pkl', 'rb') as file:
+            fc_weights = pickle.load(file)
+        with open('./Data/Approximate88/fc_biases.pkl', 'rb') as file:
+            fc_biases = pickle.load(file)
+        weights = np.zeros((2, self.slot_count))
+
+        i = 0
+        count = 0
+        while i < 6185:
+            for j in range(0, 28, 4):
+                for item in range(2):
+                    weights[item][i + j] = fc_weights[count][item]
+                count = count + 1
+            i = i + 112
+
+        self.plain_weights1 = self.ckks_encoder.encode(weights[0], self.scale)
+        self.plain_weights2 = self.ckks_encoder.encode(weights[1], self.scale)
+        self.plain_biases1 = self.ckks_encoder.encode(fc_biases[0], self.scale)
+        self.plain_biases2 = self.ckks_encoder.encode(fc_biases[1], self.scale)
+
+    # Final calculations to get output
+    def Final_Calculations(self, input_cipher):
+
+        self.evaluator.mod_switch_to_inplace(self.plain_weights1, input_cipher.parms_id())
+        self.evaluator.mod_switch_to_inplace(self.plain_weights2, input_cipher.parms_id())
+
+        out_cipher1 = self.evaluator.multiply_plain(input_cipher, self.plain_weights1)
+        out_cipher2 = self.evaluator.multiply_plain(input_cipher, self.plain_weights2)
+
+        # result1 = self.ckks_encoder.decode(self.decryptor.decrypt(out_cipher1))
+        # print("Result 1 is ", np.sum(result1))
+        # #
+        # result2 = self.ckks_encoder.decode(self.decryptor.decrypt(out_cipher2))
+        # print("Result 2 is ", np.sum(result2))
+
+        self.evaluator.relinearize_inplace(out_cipher1, self.relin_keys)
+        self.evaluator.rescale_to_next_inplace(out_cipher1)
+        out_cipher1.scale(2**50)
+
+        self.evaluator.relinearize_inplace(out_cipher2, self.relin_keys)
+        self.evaluator.rescale_to_next_inplace(out_cipher2)
+        out_cipher2.scale(2**50)
+
+        out_plain = self.ckks_encoder.encode(np.zeros(self.slot_count), self.scale)
+        out_cipher1_final = self.encryptor.encrypt(out_plain)
+        out_cipher2_final = self.encryptor.encrypt(out_plain)
+
+        self.evaluator.mod_switch_to_inplace(out_cipher1_final, out_cipher1.parms_id())
+        self.evaluator.mod_switch_to_inplace(out_cipher2_final, out_cipher2.parms_id())
+
+        # self.evaluator.add_inplace(out_cipher1_final, self.evaluator.rotate_vector(out_cipher1, 0, self.galois_keys))
+        self.evaluator.add_inplace(out_cipher1, self.evaluator.rotate_vector(out_cipher1, 3136, self.galois_keys))
+        self.evaluator.add_inplace(out_cipher1, self.evaluator.rotate_vector(out_cipher1, 1568, self.galois_keys))
+        self.evaluator.add_inplace(out_cipher1, self.evaluator.rotate_vector(out_cipher1, 784, self.galois_keys))
+
+        self.evaluator.add_inplace(out_cipher2, self.evaluator.rotate_vector(out_cipher2, 3136, self.galois_keys))
+        self.evaluator.add_inplace(out_cipher2, self.evaluator.rotate_vector(out_cipher2, 1568, self.galois_keys))
+        self.evaluator.add_inplace(out_cipher2, self.evaluator.rotate_vector(out_cipher2, 784, self.galois_keys))
+
+        i = 0
+        while i < 784:
+            self.evaluator.add_inplace(out_cipher1_final, self.evaluator.rotate_vector(out_cipher1, i, self.galois_keys))
+            self.evaluator.add_inplace(out_cipher2_final, self.evaluator.rotate_vector(out_cipher2, i, self.galois_keys))
+            i = i + 4
+            if(i % 28 == 0):
+                i = i + 84
+
+        self.evaluator.mod_switch_to_inplace(self.plain_biases1, out_cipher1_final.parms_id())
+        self.evaluator.mod_switch_to_inplace(self.plain_biases2, out_cipher2_final.parms_id())
+        self.evaluator.add_plain_inplace(out_cipher1_final, self.plain_biases1)
+        self.evaluator.add_plain_inplace(out_cipher2_final, self.plain_biases2)
+        # Level_Checker(out_cipher1_final.parms_id(), self.context)
+        # self.evaluator.square_inplace(out_cipher1_final)
+        # self.evaluator.square_inplace(out_cipher2_final)
+
+        # print("Final Outputs::::::")
+        out1 = self.ckks_encoder.decode(self.decryptor.decrypt(out_cipher1_final))
+        out2 = self.ckks_encoder.decode(self.decryptor.decrypt(out_cipher2_final))
+
+        # print("Not Squared")
+        # print(out1[0])
+        # print(out2[0])
+        out = np.zeros(2)
+        if(out1[0] > out2[0]):
+            out[0] = 1
+        else:
+            out[1] = 1
+        # print("Squared")
+        # print(out1[0] * out1[0])
+        # print(out2[0] * out2[0])
+
+        # print("Output 1 is: ", out1[6184])
+        # print("Output 2 is: ", out2[6184])
+        return out
+
+# After Huge Performance improvements and testing #
+# Everything completerd on 02/04/2022 #
+# At 8:00 PM #
